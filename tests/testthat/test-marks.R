d2a15025468a47d256087327c33c840d92c2d620
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-03 19:42:07 +0100

    Added tie_marks tests

--- tests/testthat/test-marks.R
+++ tests/testthat/test-marks.R
@@ -14,3 +14,22 @@ for(fn in names(node_marks)) {
     })
   }
 }
+
+tie_marks <- funs_objs[grepl("tie_is_", names(funs_objs))]
+for(fn in names(tie_marks)) {
+  for (ob in names(data_objs)) { 
+    test_that(paste(fn, "works on", ob), {
+      skip_if(grepl("tie_is_max|tie_is_min|tie_is_recovered", fn))
+      skip_if(grepl("tie_is_imbalanced", fn) && ob == "twomode")
+      if(fn == "tie_is_path"){
+        expect_s3_class(tie_marks[[fn]](data_objs[[ob]], 1, 2), "tie_mark")
+      } else if(fn == "tie_is_max" || fn == "tie_is_min"){
+        expect_s3_class(tie_marks[[fn]](tie_by_degree(data_objs[[ob]])), "tie_mark")
+      } else if(fn == "tie_is_infected"){
+        expect_s3_class(tie_marks[[fn]](play_diffusion(data_objs[[ob]])), "tie_mark")
+      } else {
+        expect_s3_class(tie_marks[[fn]](data_objs[[ob]]), "tie_mark")
+      }
+    })
+  }
+}
