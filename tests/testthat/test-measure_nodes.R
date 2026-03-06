91348d3d4242e0a466d52fe5954b935d94898256
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-06 18:19:53 +0100

    Testing diversity and assortativity node measures

--- tests/testthat/test-measure_nodes.R
+++ tests/testthat/test-measure_nodes.R
@@ -2,11 +2,13 @@ node_meas <- funs_objs[grepl("node_by_", names(funs_objs))]
 for(fn in names(node_meas)) {
   for (ob in names(data_objs)) { 
     test_that(paste(fn, "works on", ob), {
-      skip_if(grepl("vitality|threshold|richness|recovery|posneg|multideg|hub|homophily|hetero|exposure|diversity|distance|authority|equivalency", fn))
-      if(grepl("adoption",fn)){
-        if(ob == "diffusion"){
+      skip_if(grepl("vitality|threshold|recovery|posneg|multideg|hub|exposure|distance|authority|equivalency", fn))
+      if(grepl("diversity|richness|heterophily|homophily", fn)){
+        if(ob == "attribute")
+          expect_s3_class(node_meas[[fn]](data_objs[[ob]], "group"), "node_measure")
+      }else if(grepl("adoption",fn)){
+        if(ob == "diffusion")
           expect_s3_class(node_meas[[fn]](data_objs[[ob]]), "node_measure")
-        }
       } else if (ob %in% c("directed","undirected","weighted","twomode","signed","labelled")){
         expect_s3_class(node_meas[[fn]](data_objs[[ob]]), "node_measure")
       }
