2d406784d1c66b1fd94f2f28f875ec77ff8352e9
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-06 13:58:36 +0100

    Added cosine cluster testing

--- tests/testthat/test-model_cluster.R
+++ tests/testthat/test-model_cluster.R
@@ -1,3 +1,8 @@
 test_that("cluster_cosine works", {
-  expect_s3_class(cluster_cosine(node_by_triad(ison_monks), distance = "euclidean"), "hclust")
+  expect_s3_class(cluster_cosine(node_x_triad(ison_monks), distance = "euclidean"), "hclust")
+})
+
+test_that("cluster_cosine works", {
+  unlab_2mode <- generate_random(c(6,6))
+  expect_s3_class(cluster_concor(unlab_2mode, node_x_tetrad(unlab_2mode)), "hclust")
 })
