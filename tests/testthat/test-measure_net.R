ea6379be1c8ef0649dce41284330644e7725870c
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-13 11:15:43 +0100

    Testing balance and waves

--- tests/testthat/test-measure_net.R
+++ tests/testthat/test-measure_net.R
@@ -2,11 +2,14 @@ net_meas <- funs_objs[grepl("net_by_", names(funs_objs))]
 for(fn in names(net_meas)) {
   for (ob in names(data_objs)) { 
     test_that(paste(fn, "works on", ob), {
-      skip_if(grepl("congruency|correlation|core|change|infection|immunity|recovery|reproduction|stability|balance|strength|waves|transmiss|spatial", fn))
+      skip_if(grepl("congruency|correlation|core|infection|immunity|recovery|reproduction|change|stability|strength|transmiss|spatial", fn))
       skip_if(grepl("net_by_factions", fn) && ob == "twomode")
       if(grepl("diversity|heterophily|homophily", fn)){
         if(ob == "attribute")
           expect_s3_class(net_meas[[fn]](data_objs[[ob]], "group"), "network_measure")
+      } else if(grepl("balance", fn)){
+        if(ob == "labelled")
+          expect_s3_class(net_meas[[fn]](data_objs[[ob]]), "network_measure")
       } else {
         expect_s3_class(net_meas[[fn]](data_objs[[ob]]), "network_measure")
       }
