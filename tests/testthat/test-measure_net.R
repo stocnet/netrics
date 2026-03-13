437e6013ab56041e77586299313a71f964815629
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-13 13:34:29 +0100

    Testing net_by_strength and _toughness only once because slow

--- tests/testthat/test-measure_net.R
+++ tests/testthat/test-measure_net.R
@@ -2,7 +2,7 @@ net_meas <- funs_objs[grepl("net_by_", names(funs_objs))]
 for(fn in names(net_meas)) {
   for (ob in names(data_objs)) { 
     test_that(paste(fn, "works on", ob), {
-      skip_if(grepl("core|infection|immunity|recovery|reproduction|strength|transmiss|spatial", fn))
+      skip_if(grepl("core|infection|immunity|recovery|reproduction|transmiss|spatial", fn))
       skip_if(grepl("net_by_factions", fn) && ob == "twomode")
       if(grepl("diversity|heterophily|homophily", fn)){
         if(ob == "attribute")
@@ -13,6 +13,9 @@ for(fn in names(net_meas)) {
       } else if(grepl("congruency", fn)){
         if(ob == "twomode")
           expect_s3_class(net_meas[[fn]](data_objs[[ob]], data_objs[[ob]]), "network_measure")
+      } else if(grepl("strength|toughness", fn)){ # why is this so slow??
+        if(ob == "weighted")
+          expect_s3_class(net_meas[[fn]](data_objs[[ob]]), "network_measure")
       } else if(grepl("correlation|change|stability", fn)){
         if(ob == "labelled")
           expect_s3_class(net_meas[[fn]](data_objs[[ob]], data_objs[[ob]]), "network_measure")
