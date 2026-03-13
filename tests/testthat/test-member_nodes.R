9f4a5d93ddb8d17e7c6d03d3d85e6ff8192e2594
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-13 18:20:53 +0100

    Testing all node_members

--- tests/testthat/test-member_nodes.R
+++ tests/testthat/test-member_nodes.R
@@ -2,12 +2,20 @@ node_membs <- funs_objs[grepl("node_in_", names(funs_objs))]
 for(fn in names(node_membs)) {
   for (ob in names(data_objs)) { 
     test_that(paste(fn, "works on", ob), {
-      skip_if(grepl("equivalence|adopter|brokering", fn))
       if(grepl("roulette", fn)){
         if(ob != "twomode")
           expect_s3_class(node_membs[[fn]](data_objs[[ob]], num_groups = 3), 
+                          "node_member") else
+                            succeed("Roulette doesn't work on two-mode objects")
+      } else if(grepl("adopter", fn)){
+        if(ob == "diffusion")
+          expect_s3_class(node_membs[[fn]](data_objs[[ob]]), "node_member") else
+                            succeed("Only works on diffusion objects")
+      } else if(grepl("equivalence", fn)){
+          expect_s3_class(node_membs[[fn]](data_objs[[ob]], 
+                                           node_x_tie(data_objs[[ob]])), 
                           "node_member")
-      } else  if (ob %in% c("directed","undirected","weighted","twomode","signed","labelled")){
+      } else {
         expect_s3_class(node_membs[[fn]](data_objs[[ob]]), "node_member")
       }
     })
