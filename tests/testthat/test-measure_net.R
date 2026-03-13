15cbad3abbe22ad572a88cae7c32f88d45748be2
James Hollway <james.hollway@graduateinstitute.ch>
2026-03-13 18:48:37 +0100

    Testing net_by diffusion functions

--- tests/testthat/test-measure_net.R
+++ tests/testthat/test-measure_net.R
@@ -2,7 +2,7 @@ net_meas <- funs_objs[grepl("net_by_", names(funs_objs))]
 for(fn in names(net_meas)) {
   for (ob in names(data_objs)) { 
     test_that(paste(fn, "works on", ob), {
-      skip_if(grepl("core|infection|immunity|recovery|reproduction|transmiss|spatial", fn))
+      skip_if(grepl("core|spatial", fn))
       skip_if(grepl("net_by_factions", fn) && ob == "twomode")
       if(grepl("diversity|heterophily|homophily", fn)){
         if(ob == "attribute")
@@ -20,6 +20,10 @@ for(fn in names(net_meas)) {
         if(ob == "weighted")
           expect_s3_class(net_meas[[fn]](data_objs[[ob]]), "network_measure") else
             succeed("Testing only once because slow")
+      } else if(grepl("infection|immunity|recovery|reproduction|transmiss", fn)){
+        if(ob == "diffusion")
+          expect_s3_class(net_meas[[fn]](data_objs[[ob]]), "network_measure") else
+            succeed("Only used for diffusion objects")
       } else if(grepl("correlation|change|stability", fn)){
         if(ob == "labelled")
           expect_s3_class(net_meas[[fn]](data_objs[[ob]], data_objs[[ob]]), "network_measure") else
